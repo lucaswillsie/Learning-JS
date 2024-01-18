aaece96e44607924a5db02079c740c13780edf13
Lucas Willsie <60862306+lucaswillsie@users.noreply.github.com>
2024-01-17 22:08:49 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+function changeMsg() {
+    document.getElementById("heading").innerHTML = "Fuck Joe Biden";
+    var heading = document.getElementById("heading");
+    heading.setAttribute("class", "makeRed");
+}
+
