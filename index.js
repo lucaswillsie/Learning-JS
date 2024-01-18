d453e9fff945a6116183d5ebdcc88a91899cacc0
Lucas Willsie <60862306+lucaswillsie@users.noreply.github.com>
2024-01-17 23:16:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 function changeMsg() {
-    document.getElementById("heading").innerHTML = "Fuck Joe Biden";
+    document.getElementById("heading").innerHTML = "Without js";
     var heading = document.getElementById("heading");
     heading.setAttribute("class", "makeRed");
 }
